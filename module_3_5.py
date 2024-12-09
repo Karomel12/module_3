9fb201aa79d2447890d932c16f6036384dba8048
Денис <denisboarsci@gmail.com>
2024-12-09 21:05:48 +0500

    Done: 8.12.24 fast fix

--- module_3_5.py
+++ module_3_5.py
@@ -1,8 +1,8 @@
 def get_multiplied_digits (number):
     str_number = str(number)
     first = int(str_number[0])
-    if str_number.endswith('0'):
-        str_number = str_number[:len(str_number) - 1]
+    while str_number.endswith('0'):
+            str_number = str_number[:len(str_number)-1]
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
     first = int(str_number)
@@ -11,4 +11,6 @@ def get_multiplied_digits (number):
 result = get_multiplied_digits(40203)
 print(result)
 result2 = get_multiplied_digits(402030)
+print(result2)
+result2 = get_multiplied_digits(100)
 print(result2)
